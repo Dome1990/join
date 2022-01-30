82ec57eb88321aa1d56ae521574645f586f7a163
Marvin Wiemers <marvinwiemers@gmail.com>
2022-01-30 11:53:25 +0100

    update backlog message

--- js/backlog.js
+++ js/backlog.js
@@ -25,28 +25,33 @@ async function filterTasksForBacklog() {
 
 function showTasksInBacklog(backlog) {
     numberOfAssignedPeople(backlog);
-    for (let i = 0; i < backlog.length; i++) {
-        document.getElementById('tasks').innerHTML += `
-     <div onclick="updateStatusToBoard(${backlog[i]['id']})" id="task${i}" class="backlogDetail__container background__lightblue" style="border-left:16px solid ${backlog[i]['personalColor'][0]}">
-         <div id="mobile__left__container">
-        <div class="left__container">
-             <div class="img__container">
-                <img class="images" src=${backlog[i]['img'][0]} alt="./img/user.png ">
+    if(backlog.length == 0){
+        document.getElementById('tasks').innerHTML = `<p>No task to show. Please add some task in "add task". If you need help, then change to "help".</p>`;
+    }else{
+        for (let i = 0; i < backlog.length; i++) {
+            document.getElementById('tasks').innerHTML += `
+         <div onclick="updateStatusToBoard(${backlog[i]['id']})" id="task${i}" class="backlogDetail__container background__lightblue" style="border-left:16px solid ${backlog[i]['personalColor'][0]}">
+             <div id="mobile__left__container">
+            <div class="left__container">
+                 <div class="img__container">
+                    <img class="images" src=${backlog[i]['img'][0]} alt="./img/user.png ">
+                 </div>
+             <div class="assignedTo "title="${backlog[i]['assignedTo']}">
+                 <p id="name" >${backlog[i]['assignedTo'][0]}  ${assignedPeople[i]}</p>
+                 <p id="email" class="blue">${backlog[i]['email'][0]}</p>
              </div>
-         <div class="assignedTo "title="${backlog[i]['assignedTo']}">
-             <p id="name" >${backlog[i]['assignedTo'][0]}  ${assignedPeople[i]}</p>
-             <p id="email" class="blue">${backlog[i]['email'][0]}</p>
          </div>
-     </div>
-     <div class="category__container">
-         <p>${backlog[i]['category']}</p>
-     </div>
-     </div>
-     <div class="details__container">
-         <p>${backlog[i]['description']}</p>
-      </div>
- </div>`;
+         <div class="category__container">
+             <p>${backlog[i]['category']}</p>
+         </div>
+         </div>
+         <div class="details__container">
+             <p>${backlog[i]['description']}</p>
+          </div>
+     </div>`;
+        }  
     }
+    
 }
 /**
  * counts number of assigned People and returns it as string
